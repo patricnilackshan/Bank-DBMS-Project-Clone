f9b9993bcb709555f32eba987f237e7e7a805b22
Patric Nilackshan <126662734+patricnilackshan@users.noreply.github.com>
2023-12-03 05:31:28 +0530

    DB and Inital Files

---
+++ sql/current_working/datadumping.sql
@@ -0,0 +1,270 @@
+SET foreign_key_checks = 0;
+
+
+--
+-- Dumping data 
+--
+
+INSERT INTO web_platform_user VALUES
+  ('BR02M1','$2b$10$fzu2c2dzsis5Qzob81VdcOv3qXd3SHqLN7/d06/5vcEDaUTdPrdPK', 'elensmith@yahoo.com', '2023-10-20 21:23:00'),
+  ('BR01M1','$2b$10$7V4C/c8SHvvoigr/ItFFTOL9/qL1.kMm.APdbkN2peqelh1zIY7Q6', 'peterparker@gmail.com', '2023-10-15 09:23:55'),
+  ('BR01M2','$2b$10$WQFv.pzpP.CiZNZPbmv01uAWpcFOsMlBLXtVoO3keY.RPjt4B035W', 'jadewarner@gmail.com', '2023-10-18 20:42:55'),
+  ('BR03M1','$2b$10$QyLg/ZGS0SlmxCjnY.G4o.PEV1W.qykfpa6vueP8FgGhheJF4Jev6', 'nancywhite@gmail.com', '2023-10-24 06:23:02'),
+  ('BR04M1','$2b$10$wxorL7ZDUy8/dJs4Ogk24OAN0zN3UN0ne3wRDHltj8nZ0Pj.LywHS', 'lucastaylor@gmail.com', '2023-10-09 13:42:30'),
+  ('BR03M2','$2b$10$42EPP//pW8k2NIKCv1SwZOiqJ5.nusSLJ8B5/w4AfaL1sWBoky5GC', 'markjohnson@gmail.com', '2023-10-28 18:23:40'),
+  ('BR05M1','$2b$10$JDJOm6djZe3GrtEvFMfRgeqVz9pSTv4V6DGSAJRHgyLx.wp3T4vZy', 'lilywillstone@gmail.com', '2023-10-04 22:31:55'),
+  ('BR05M2','$2b$10$BWU2KLRm2sxKeu.N27biVO6L6PO6nmy/5C8aL8XhhvDNpOYGqEKA2', 'stevenroot@gmail.com', '2023-10-15 23:23:30'),
+  ('BR05M3','$2b$10$pB6Dv2E.wxwtCNwuDKtvNex0ulVCmjbwEyqLVlwTNP7Ymy65GJsKS', 'alliewilliamson@gmail.com', '2023-10-12 07:52:55'),
+  ('BR02M2','$2b$10$XTrbkIVqur9VsklRayzHD.mxpsP5COHJFuuQ1ym6zLwFXDYQPQUci', 'eloncummins@gmail.com', '2023-10-28 10:23:00'),
+  ('BR02M3','$2b$10$C/WwzC2lLK5wZiAHpE37.e4cghZwuF6IZWGFH8JHCbEhLTqP.YAGW', 'maddywade@gmail.com', '2023-10-11 11:31:01'),
+  ('BR03M3','$2b$10$iLWfa0e5MD1zcjQTbBCh3.wWxSueU8ITIWjopYBWMjzSpnroOAeB2', 'tomstark@gmail.com', '2023-10-08 12:42:40'),
+  ('BR01M3','$2b$10$Kg48uwfYFmM0IpO/t3qkhee4W5qwnBzLnxUhoSs1gFsJxy6UdePqq', 'drakebrook@gmail.com', '2023-10-20 09:20:55'),
+  ('BR04M2','$2b$10$k8ImAR4.r2wkpyB8VopPcec6RnmpIU.Oo6Rwq50584.ZywxogxjbG', 'felixevens@gmail.com', '2023-10-15 09:23:35'),
+  ('AB2001','$2b$10$l2HOrIIAWdgNphsjHgoMfe9XyynTRK16PrtJmdoESNsVxjlp5u90.', 'johnnybrown@gmail.com', '2023-10-15 09:23:35'),
+  ('PCR002','$2b$10$bna3yhXvHq6cD4iEGmGJv.tbQJZLabnnnLdJ61jlA3N0cTJ1ZL6u6', 'catalyst@gmail.com', '2023-10-15 09:23:35'),
+  ('GTA562','$2b$10$N3pa7TS5OBlFazs.13IMLe2O3VYjEq6OqORbA3bdmgFf47/.hAh.K', 'alicesmith@gmail.com', '2023-10-30 13:50:35'),
+  ('129AC56','$2b$10$MlK/t1ORGdxkxLP3gJbk5uls76/nUmztR64jbpMgqNWEJkDS1ngZG', 'jamesmiller@gmail.com', '2023-10-23 23:23:35'),
+  ('PLC5600','$2b$10$3kXSJoXHmKFQaYQXIPFa1O/7Ud4740FC2Dc/J4s761A4EMX.PkAEK', 'avawilliams@gmail.com', '2023-10-15 09:45:35'),
+  ('0012GB','$2b$10$aoVYhjyFJc1lINuDMmM2oOgi21xD4pTYs2o.L15hDkO7JwrxXhV8S', 'lucasmartinez@gmail.com', '2023-10-21 06:22:35'),
+  ('LN2453','$2b$10$H1yhSkr7LIsvRhO/4JLP6eLmKUkYpXkyIH4DT2NQJHOFIwBcabg4i', 'nexusltd@gmail.com', '2023-10-17 09:34:35'),
+  ('KGP02','$2b$10$plxkJmtPh6hvAqguYquCbel2w1vGk7sG3q48E5NqN3ny91.qZbxeO', 'robloxcoop@gmail.com', '2023-10-15 10:42:35'),
+  ('29300L','$2b$10$.IxXivjiHnKRh1CUvrnUaeZWq44L5xQqhkEVMza3wN7l7HKZKUv.O', 'michaeljohnson@gmail.com', '2023-09-15 11:23:55'),
+  ('D34200','$2b$10$kKoGqjokoEWI1znuAIRYM.9lsveCf6IOiBv.6MTw9kBBCNERjeu2u', 'ameliawhite@gmail.com', '2023-10-15 12:39:35'),
+  ('98000F','$2b$10$KXUycbW3XI1r7uZksOmqeuXPlpwLg04jnAYhY4iNm7hpeXw9spGIO', 'williamssons@gmail.com', '2023-10-08 07:40:23'),
+  ('X000FX','$2b$10$v/NXrfLHso5OEQT7X85J0udkPQ1PT2tUpjRjc8n6AbjPRGHsWKYOi', 'benjamintaylor@gmail.com', '2023-10-23 09:23:49'),
+  ('F112U2','$2b$10$Zp989kwOuBiyMGmsTgs6Ju0vE9QMbJw8PlMwtwFFleYm6JeQDSwdy', 'johndoe@gmail.com', '2023-10-15 09:23:35'),
+  ('L0P123','$2b$10$d0W/rPuFmm3fDIPq0ygIAuQtdAcahl10HUoQ2vtYiURovr3i3nJW.', 'loganmartinez@gmail.com', '2023-04-14 04:56:23'),
+  ('R2D2C3','$2b$10$jSOLWlVRtplKeVfkRGO0uOJdkrSdg6ghUJvEZ9Kd/66QTymxYRqPm', 'drakeco@gmail.com', '2023-08-19 10:45:35'),
+  ('BD3200','$2b$10$oR0dIzojI86vY9LMkzyMZu2j5ZaLkDUBCFO4TveQYUQRrs9bj7/1u', 'jimmywilliams@gmail.com', '2023-06-15 11:28:35'),
+  ('JH78Y49','$2b$10$Vhgj1TX2pKVYhChhICPoXOSTdUlO8PONysGTjpp8bZOuxRLMTjmKC', 'annemiller@gmail.com', '2023-10-17 09:46:56'),
+  ('00H2P3','$2b$10$6NpSkGAh6FNt8j1Zo6638.GJ2CQas.8g.WvrJNWcHdRpdiYMX9Gv6', 
+'alicejones@gmail.com', 
+'2023-11-28 13:15:45'),
+('admin',
+"$2b$10$o9ZaqokwN4eBEjDjWYD7/uRCWFl/.CHbz4ykdTHFaLyzUzJoB1QaK",
+'admin@gmail.com',
+'2023-11-01 01:10:20'
+);
+-- admin password == "password"
+
+INSERT INTO branch VALUES
+  (1001, 'Anse Boileau', '1234567890', '123, Main Street, Anse Boileau, Seychelles', 'anseboileau@abank.com'),
+  (1002, 'Glacis', '9876543210', '456, Elm Street, Glacis, Seychelles', 'glacis@abank.com'),
+  (1003, 'Plaisance', '3456789012', '789, Oak Street, Plaisance, Seychelles ', 'plaisance@abank.com'),
+  (1004, 'Takamaka', '7890123456', '1011, Maple Street, Takamaka, Seychelles', 'takamaka@abank.com'),
+  (1005, 'Victoria', '5678901234', '1213, Pine Street, Victoria, Seychelles', 'victoria@abank.com'); 
+
+-- Position and salary are NULL if the employee is retired.
+INSERT INTO employee VALUES
+  (4001, 'Peter', 'Parker', 1001, '87681006780V', '0662300931', 'Victoria, Seychelles, 10010', '1997-08-29', 'M', 'BR01M1','Head_Manager', 70000.00, 'Active'),
+  (4002, 'Elen', 'Smith', 1002, '90028293749V', '0679200000', 'Providence Atoll, Seychelles, 19942', '1985-12-20', 'F', 'BR02M1','Manager', 60000.00, 'Active' ),
+  (4003, 'Jade', 'Warner', 1001, '30003268394V', '0662200003', 'Cosmoledo Atoll, Seychelles, 24542', '1980-03-10', 'M', 'BR01M2','Manager', 65000.00, 'Active'),
+  (4004, 'Nancy', 'White', 1003, '20037373827V', '0677000000', 'Astove Island, Seychelles, 47653', '1975-08-29', 'F', 'BR03M1','Manager', 100000.00, 'Active'),
+  (4005, 'Lucas', 'Taylor', 1004,  '10107468239V', '0678844445', 'Alphonse Island, Seychelles, 19928', '1980-10-22', 'M', 'BR04M1','Manager', 85000.00, 'Active'),
+  (4006, 'Mark', 'Johnson', 1003, '88000066219V', '0665444400', 'Desroches Island, Seychelles, 19922', '1983-08-25', 'M', 'BR03M2', NULL, NULL, 'Retired'),
+  (4007, 'Lily', 'Willstone', 1005, '33000047620V', '0662444449', 'Denis Island, Seychelles, 16524', '1996-09-30', 'F', 'BR05M1','Manager', 75000.00, 'Active'),
+  (4008, 'Steven', 'Root', 1005, '30004847620V', '0662435777', 'Astove Island, Seychelles, 45672', '1998-12-29', 'M', 'BR05M2','Staff', 40000.00, 'Active'),
+  (4009, 'Allie', 'Willamson', 1005, '44444847620V', '0677735249', 'Providence Atoll, Seychelles, 19929', '1999-04-01', 'F', 'BR05M3','Staff', 55000.00, 'Active'),
+  (4010, 'Elon', 'Cummins', 1002, '37272847620V', '0662477749', 'Denis Island, Seychelles, 16466', '2000-02-10', 'M', 'BR02M2','Staff', 30000.00, 'Active'),
+  (4011, 'Maddy', 'Wade', 1002, '39922847620V', '0662435333', 'Alphonse Island, Seychelles, 13382', '1980-01-19', 'F', 'BR02M3','Staff', 40000.00, 'Active'),
+  (4012, 'Tom', 'Stark', 1003, '33811117620V', '0662433339', 'Providence Atoll, Seychelles, 14490', '1988-09-24', 'M', 'BR03M3', NULL, NULL, 'Retired'),
+  (4013, 'Drake', 'Brook', 1001, '33881111620V', '0662435222', 'Desroches Island, Seychelles, 24400', '1978-10-23', 'M', 'BR01M3','Staff', 52000.00, 'Active'),
+  (4014, 'Felix', 'Evans', 1004, '23000006270V', '0672222200', 'Bird Island, Seychelles, 21880', '1985-01-03', 'M', 'BR04M2','Staff', 35000.00, 'Active');
+
+INSERT INTO branch_manager VALUES (1001, 4001), (1002, 4002), (1003, 4004), (1004, 4005), (1005, 4007);
+
+INSERT INTO customer VALUES
+  (10001, 'Individual', 'AB2001'),
+  (10002, 'Organization', 'PCR002'),
+  (10003, 'Individual', 'GTA562'),
+  (10004, 'Individual', '129AC56'),
+  (10005, 'Individual', 'PLC5600'),
+  (10006, 'Individual', '0012GB'),
+  (10007, 'Organization', 'LN2453'),
+  (10008, 'Organization', 'KGP02'),
+  (10009, 'Individual', '29300L'),
+  (10010, 'Individual', 'D34200'),
+  (10011, 'Organization', '98000F'),
+  (10012, 'Individual', 'X000FX'),
+  (10013, 'Individual', 'F112U2'),
+  (10014, 'Individual', '00H2P3'),
+  (10015, 'Organization', 'R2D2C3'),
+  (10016, 'Individual', 'BD3200'),
+  (10017, 'Individual', 'JH78Y49'),
+  (10018, 'Individual', 'L0P123');
+
+INSERT INTO individual VALUES
+  (10001, 'Johnny', 'Brown', '92381006780V', '0662375931', 'Victoria, Seychelles, 16112', '1940-08-29', 'M'),
+  (10003, 'Alice', 'Smith', '43728293749V', '0679289429', 'Providence Atoll, Seychelles, 24190', '1983-10-20', 'F'),
+  (10004, 'James', 'Miller', '98435268394V', '0662200003', 'Cosmoledo Atoll, Seychelles, 24180', '1930-04-10', 'M'),
+  (10005, 'Ava', 'Williams', '19997373827V', '0677772000', 'Astove Island, Seychelles, 24170', '2004-08-15', 'F'),
+  (10006, 'Lucas', 'Martinez', '23387468239V', '0672344445', 'Alphonse Island, Seychelles, 24160', '1980-10-22', 'M'),
+  (10009, 'Michael', 'Johnson', '88236766219V', '0665550000', 'Desroches Island, Seychelles, 24150', '1980-08-25', 'M'),
+  (10010, 'Amelia', 'White', '33884847620V', '0662435249', 'Denis Island, Seychelles, 24140', '1978-09-10', 'F'),
+  (10012, 'Benjamin', 'Taylor', '23004726270V', '0672210000', 'Bird Island, Seychelles, 24139', '1959-01-03', 'M'),
+  (10013, 'John', 'Doe', '99473728888V', '0662375930', 'Victoria, Seychelles, 12008', '2007-09-30', 'M'),
+  (10014, 'Alice', 'Jones', '43738200050V', '0679289430', 'Providence Atoll, Seychelles, 24191', '1943-08-21', 'F'),
+  (10016, 'Jimmy', 'Williams', '90000268395V', '0662200004', 'Cosmoledo Atoll, Seychelles, 24181', '1930-04-11', 'M'),
+  (10017, 'Anne', 'Miller', '19030000828V', '0670072001', 'Astove Island, Seychelles, 24171', '2006-08-16', 'F'),
+  (10018, 'Logan', 'Martinez', '20090468240V', '0672344446', 'Alphonse Island, Seychelles, 24161', '2018-10-23', 'M');
+
+INSERT INTO organization VALUES
+  (10002, 1000, 'Catalyst Pvt LTD', '0465323100', 'Catalyst Pvt LTD, Victoria, Seychelles'),
+  (10007, 9300, 'The Nexus LTD', '0442838920', 'The Nexus, Alphonse Island, Seychelles'),
+  (10008, 5400, 'Roblox Co-operation', '0463000200', 'Roblox Co-operation, Plaisance, Seychelles'),
+  (10011, 3400, 'Williams and Sons', '0443627002', 'Williams and Sons, Glacis, Seychelles'),
+  (10015, 5000, 'Drake and Co', '0463000200', 'Drake and Co, Takamaka, Seychelles');
+
+INSERT INTO organization_individual VALUES
+	(1000, 10005, 'Secretary'),
+	(9300, 10018, 'Treasurer'),
+	(5400, 10009, 'Leader'),
+	(3400, 10014, 'Vice Secretary'),
+	(5000, 10003, 'Treasurer');
+
+INSERT INTO account VALUES 
+  (2300021, 10001, 1001, 400000.00,'2016-11-11', 'Active', 'Saving', 3 ),
+  (1002734, 10002, 1001, 50000.00,'2019-02-20', 'Active', 'Checking', 18 ),
+  (6400028, 10003, 1003, 300000.00,'2017-07-11', 'Active', 'Saving', 1 ),
+  (5000293, 10004, 1002, 103050.00,'2018-12-24', 'Active', 'Saving', 2 ),
+  (9340000, 10005, 1001, 350750.00,'2020-04-11', 'Active', 'Saving', 3 ),
+  (1837927, 10006, 1002, 100.00,'2020-08-04', 'Suspended', 'Checking', 3 ),
+  (6400022, 10007, 1004, 250000.00,'2020-11-15', 'Active', 'Saving', 4 ),
+  (7223913, 10008, 1003, 120000.00,'2020-12-31', 'Active', 'Checking', 14 ),
+  (1234567, 10009, 1005, 1030500.00,'2021-05-11', 'Active', 'Checking', 7 ),
+  (9876543, 10010, 1004, 0.00,'2021-06-23', 'Closed', 'Saving', 5 ),
+  (6543210, 10011, 1001, 400750.00,'2021-11-09', 'Active', 'Saving', 0 ),
+  (6003002, 10012, 1005, 1500000.00,'2022-09-11', 'Active', 'Saving', 4 ),
+  (5000203, 10013, 1003, 0.00,'2022-08-29', 'Closed', 'Saving', 1 ),
+  (2353647, 10014, 1003, 3500000.00,'2023-04-11', 'Active', 'Checking', 12 ),
+  (2367647, 10015, 1001, 0.00,'2023-05-28', 'Closed', 'Saving', 2 ),
+  (6059430, 10016, 1002, 2300000.00,'2023-06-29', 'Active', 'Checking', 1 ),
+  (2234649, 10017, 1004, 750000.00,'2023-07-03', 'Active', 'Saving', 0 ),
+  (2353560, 10018, 1005, 800000.00,'2023-08-04', 'Active', 'Saving', 2);
+
+INSERT INTO savings_plan VALUES
+  ('Senior', 0.13, 1000.00, 5),
+  ('Adult', 0.10, 1000.00, 5),
+  ('Teen', 0.11, 500.00, 5),
+  ('Children', 0.12, 0.00, 5);
+
+-- Next payment date is NULL if the account is suspended or closed.
+INSERT INTO savings_account VALUES
+  (2300021, '2023-11-11', 'Senior'),
+  (6400028, '2023-11-11', 'Adult'),
+  (5000293, '2023-11-24', 'Senior'),
+  (9340000, '2023-11-11', 'Teen'),
+  (6400022, '2023-11-11', 'Adult'),
+  (9876543, NULL, 'Adult'),
+  (6543210, '2023-11-09', 'Adult'),
+  (6003002, '2023-11-11', 'Senior'),
+  (5000203, NULL, 'Teen'),
+  (2367647, NULL, 'Adult'),
+  (2234649, '2023-11-03', 'Teen'),
+  (2353560, '2023-11-04', 'Children');
+
+INSERT INTO checking_account VALUES 
+  (1002734),
+  (1837927),
+  (1234567),
+  (7223913),
+  (2353647),
+  (6059430);
+
+INSERT INTO fd_plan VALUES
+  (7001, 0.13, 6),
+  (7002, 0.14, 12),
+  (7003, 0.15, 36);
+
+INSERT INTO fd VALUES
+  (123456, 1, 2300021, 1004, '2023-08-11', '2023-11-11', 1000000.00, 'Active', '2024-02-11'),
+  (400050, 2, 5000293, 1001, '2022-05-20', NULL, 250000.00, 'Matured', '2023-05-20'),
+  (983400, 3, 9340000, 1002, '2020-06-29', NULL, 3000000.00, 'Withdrawn', '2023-06-29'),
+  (720043, 2, 6003002, 1005, '2023-04-05', '2023-11-05', 475000.00, 'Active', '2026-04-05'),
+  (567229, 1, 6400028, 1003, '2023-10-01', '2023-11-01', 550000.00, 'Active', '2024-04-01');
+
+INSERT INTO transaction VALUES
+  (970001, 5000.00, 'Deposit', 'Mahapola', '2023-08-04 23:10:10'),
+  (970002, 500.00, 'Deposit', 'Returning reload', '2021-05-30 10:08:40'),
+  (970003, 1000.00, 'Withdrawal', 'Online Transfer', '2022-08-29 18:32:56'),
+  (970004, 1000.00, 'Deposit', 'Online Transfer', '2022-08-29 18:32:56'),
+  (970005, 650.00, 'Deposit', 'Monthly interest', '2023-10-01 12:46:10'),
+  (970006, 400.00, 'Withdrawal', NULL, '2023-01-01 21:11:28'),
+  (970007, 300.00, 'Withdrawal', 'Online Transfer', '2023-03-15 10:54:35'),
+  (970008, 300.00, 'Deposit', 'Online Transfer', '2023-03-15 10:54:35'),
+  (970009, 1400.00, 'Withdrawal', NULL, '2023-03-21 16:32:01'),
+  (970010, 2500.00, 'Deposit', 'CDM Deposit', '2023-04-26 13:43:26'),
+  (970011, 10000.00, 'Withdrawal', 'Online Transfer', '2023-07-29 14:45:47'),
+  (970012, 10000.00, 'Deposit', 'Online Transfer', '2023-07-29 14:45:47'),
+  (970013, 55000.00, 'Withdrawal', NULL, '2023-10-15 09:23:55'); 
+
+INSERT INTO deposit VALUES
+  (970001, 2300021),
+  (970002, 6003002),
+  (970004, 6003002),
+  (970005, 6543210),
+  (970008, 7223913),
+  (970010, 9340000),
+  (970012, 2300021); 
+
+INSERT INTO withdrawal VALUES
+  (970003, 5000293),
+  (970006, 7223913),
+  (970007, 2300021),
+  (970009, 2353647),
+  (970011, 6543210),
+  (970013, 6003002); 
+
+INSERT INTO loan VALUES
+  (90001, 10004, 5000293, 1001, 'Personal', 100000.00, '2022-05-28', '2023-05-28' ),
+  (90002, 10016, 6059430, 1005, 'Personal', 200000.00, '2022-06-30', '2023-06-30' ),
+  (90003, 10002, 1002734, 1001, 'Business', 400000.00, '2023-07-03', '2026-07-03' ),
+  (90004, 10017, 2234649, 1004, 'Personal', 200000.00, '2023-08-05', '2025-08-05' ),
+  (90005, 10003, 6400028, 1002, 'Personal', 250000.00, '2023-08-14', '2024-08-14' ),
+  (90006, 10001, 2300021, 1003, 'Personal', 400000.00, '2023-08-22', '2025-08-22' ),
+  (90007, 10012, 6003002, 1005, 'Personal', 200000.00, '2023-09-15', '2026-09-15' ); 
+
+INSERT INTO online_loan VALUES (90006, 123456), (90007, 720043);
+
+INSERT INTO loan_request VALUES
+  (70001, 10004, 5000293, 1001, TRUE, 100000.00, 'Personal'),
+  (70002, 10009, 1234567, 1004, FALSE, 500000.00, 'Business'),
+  (70003, 10016, 6059430, 1005, TRUE, 200000.00, 'Personal'),
+  (70004, 10010, 9876543, 1003, FALSE, 300000.00, 'Personal'),
+  (70005, 10002, 1002734, 1001, TRUE, 400000.00, 'Business'),
+  (70006, 10007, 6400022, 1003, FALSE, 300000.00, 'Business'),
+  (70007, 10017, 2234649, 1004, TRUE, 200000.00, 'Personal'),
+  (70008, 10003, 6400028, 1002, TRUE, 250000.00, 'Personal');
+
+INSERT INTO offline_loan VALUES
+  (90001, 70001), (90002, 70003), (90003, 70005), (90004, 70007), (90005, 70008);
+
+INSERT INTO loan_installment VALUES
+  (90003, 11111.11, '2023-11-03', NULL),
+  (90004, 8333.33, '2023-09-05', NULL),
+  (90005, 20833.33, '2023-10-14', '2023-10-18'),
+  (90006, 16666.66, '2023-09-22', NULL),
+  (90007, 5555.55, '2023-10-15', '2023-10-14');
+
+INSERT INTO loan_arrears VALUES (90004, 9228.99), (90006, 18457.97);
+
+INSERT INTO report VALUES
+  (30001, 'Transaction Report', '2023-09-29 14:45:24', 4001),
+  (30002, 'Transaction Report', '2023-10-03 02:20:18', 4002),
+  (30003, 'Transaction Report', '2023-10-24 08:38:15', 4004),
+  (30004, 'Transaction Report', '2023-10-26 10:39:29', 4005),
+  (30005, 'Transaction Report', '2023-10-28 13:18:38', 4007),
+  (30006, 'Loan Installment Report', '2023-09-30 18:45:55', 4003),
+  (30007, 'Loan Installment Report', '2023-09-30 14:10:41', 4002),
+  (30008, 'Loan Installment Report', '2023-09-30 15:28:50', 4007),
+  (30009, 'Loan Installment Report', '2023-09-30 09:37:20', 4005),
+  (30010, 'Loan Installment Report', '2023-09-30 21:54:05', 4004),
+  (30011, 'Transaction Report', '2023-07-29 14:43:47', 4004),
+  (30012, 'Transaction Report', '2023-07-29 14:32:00', 4007),
+  (30013, 'Transaction Report', '2023-07-29 14:21:41', 4005),
+  (30014, 'Transaction Report', '2023-07-29 14:01:27', 4003),
+  (30015, 'Transaction Report', '2023-07-29 14:25:01', 4002),
+  (30016, 'Loan Installment Report', '2023-09-30 14:37:00', 4003);
+
+
+SET foreign_key_checks = 1;
